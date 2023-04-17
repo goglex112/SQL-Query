4d534ae38bf0252f3a7bd294971c3dc1698b0734
Ronald Salim <92841420+goglex112@users.noreply.github.com>
2023-04-17 15:01:55 +0700

    Update High Energy Consumption.sql

--- High Energy Consumption.sql
+++ High Energy Consumption.sql
@@ -1,3 +1,7 @@
+--Find the date with the highest total energy consumption from the Meta/Facebook data centers. 
+--Output the date along with the total energy consumption across all data centers.
+--Tables: fb_eu_energy, fb_asia_energy, fb_na_energy
+
 select date, consumption from
     (select date, sum(consumption) as consumption, dense_rank() over(order by sum(consumption) desc) as rank from
             (select * from public.fb_eu_energy
